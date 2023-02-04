4a7ac32a7a809ddfcc9ffa7583c988db80c9213f
AnamulHassan <itsanamulhassan@gmail.com>
2023-02-04 07:51:11 +0600

    fix some ui issues

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
       <span className="text-white text-5xl font-bold leading-5">App</span>
       <button
         onClick={() => handleLogout()}
-        className="bg-[#9BFF00] py-3 px-6 rounded-[40px] text-[#050505] text-base leading-5 flex items-center space-x-1"
+        className="bg-[#9BFF00] py-3 hover:bg-opacity-80 duration-300 px-6 rounded-[40px] text-[#050505] text-base leading-5 flex items-center space-x-1"
       >
         <BiUser className="text-[#696969] text-xl" /> Logout
       </button>
